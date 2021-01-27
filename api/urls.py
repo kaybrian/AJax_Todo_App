03c36a0f0ea0474cb288915cdd6c7f7368caa6aa
brian <kayonbrian@gmail.com>
2021-01-27 20:35:20 +0300

    succesfully done the crud in django

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     path('',views.apiOverview,name="apioverview"),
-    path('task-list/',views.tasklist,name='task-list')
+    path('task-list/',views.tasklist,name='task-list'),
+    path('task-detail/<str:pk>/',views.taskDetail,name='task-detail'),
+    path('task-create/',views.taskCreate,name='task-create'),
+    path('task-update/<str:pk>/',views.taskUpdate,name='task-update'),
+    path('task-delete/<str:pk>/',views.taskDelete,name='task-delete'),
 ]
 
 
