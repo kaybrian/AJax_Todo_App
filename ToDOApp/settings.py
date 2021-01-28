585c659a78be5b039acd24da5af6b8cff22920b6
brian <kayonbrian@gmail.com>
2021-01-28 11:02:28 +0300

    added the form ajax forms

--- ToDOApp/settings.py
+++ ToDOApp/settings.py
@@ -1,4 +1,4 @@
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -113,3 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def List(request):
+    context = {}
+    return render(request,'frontend/index.html',context)
